e6b5d18a0244eaf5de7a2e1a1630668f7224113f
luoqiang <luoqiang314@163.com>
2017-04-19 11:52:34 +0800

    管理员列表与分页

--- application/admin/controller/admin.php
+++ application/admin/controller/admin.php
@@ -1,16 +1,37 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use app\admin\model\Admin as adminModel;
+use think\Db;
 
 class Admin extends Controller
 {
     public function lst()
     {
+        $list = AdminModel::paginate(3);
+        $this -> assign('list', $list);
         return $this->fetch();
     }
 
     public function add()
     {
+        if (request()->isPost()) {
+            $data = [
+                'username' => input('username'),
+                'password' => (input('password'))
+            ];
+            $validate = \think\Loader::validate('Admin');
+            if(!$validate->scene('add')->check($data)){
+                $this->error($validate->getError());
+                die;
+            }
+           if(Db::name('admin')->insert($data)){
+               return $this->success('添加管理员成功！', 'lst');
+           }else{
+               return $this->error('添加管理员失败！');
+
+           }
+        }
         return $this->fetch();
     }
 
---
+++ application/admin/validate/Admin.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\admin\validate;
+use think\Validate;
+
+class Admin extends Validate
+{
+    protected $rule = [
+        'username' => 'require|max:25',
+        'password' => 'require|max:25'
+    ];
+
+    protected $message = [
+        'username.require' => '管理员名称必须填写',
+        'username.max' => '管理员名称长度不能大于25位',
+        'password.reuire' => '管理员密码必须填写'
+    ];
+
+    protected $scene = [
+        'add' => ['username']
+    ];
+}
